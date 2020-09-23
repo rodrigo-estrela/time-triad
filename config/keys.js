07631dad56465a2fef3068e83209e96d5117e2fd
Rodrigo Estrela <rodrigo_estrela@hotmail.com>
2020-09-22 23:56:29 -0300

    finished auth flow

--- config/keys.js
+++ config/keys.js
@@ -1,7 +1,5 @@
-module.exports = {
-  googleClientID:
-    "455587037771-imi7lmpqfi4nkkv2tu2v87akv2k67ort.apps.googleusercontent.com",
-  googleClientSecret: "9U5UZwZyGpvSwofCwc7zhwLI",
-  mongoURI:
-    "mongodb+srv://rodrigoestrela:rse.ckxc21@final-project-dev.op3ej.mongodb.net/final-project-dev?retryWrites=true&w=majority",
-};
+if (process.env.NODE_ENV === "production") {
+  module.exports = require("./prod");
+} else {
+  module.exports = require("./dev");
+}
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -9,4 +9,18 @@ module.exports = (app) => {
   );
 
   app.get("/auth/google/callback", passport.authenticate("google"));
+
+  app.get("/api/logout", (req, res) => {
+    req.logout();
+    res.send(req.user);
+  });
+
+  app.get("/api/current_user", (req, res) => {
+    res.send(req.user);
+  });
 };
+
+// mongodb+srv://rodrigoestrela:Rse.ckxc29@final-project-prod.0gwy5.mongodb.net/final-project-prod?retryWrites=true&w=majority
+
+// ClientID: 323395734845-fdikmt8k52o1fehu58o8h6uk8pa3h7tb.apps.googleusercontent.com
+// ClientSecret: QrNclhDG1AkzOJuip7Xb6j8x
