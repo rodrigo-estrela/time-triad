fef2c0b2b9151dfb6c68a6904480f51b151c9277
Rodrigo Estrela <rodrigo_estrela@hotmail.com>
2020-09-22 22:33:38 -0300

    initial commit

---
+++ config/keys.js
@@ -0,0 +1,7 @@
+module.exports = {
+  googleClientID:
+    "455587037771-imi7lmpqfi4nkkv2tu2v87akv2k67ort.apps.googleusercontent.com",
+  googleClientSecret: "9U5UZwZyGpvSwofCwc7zhwLI",
+  mongoURI:
+    "mongodb+srv://rodrigoestrela:rse.ckxc21@final-project-dev.op3ej.mongodb.net/final-project-dev?retryWrites=true&w=majority",
+};
